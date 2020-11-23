f4ab5cd919affc245fe8ae8cf77b08ca2966f4e6
kelvin <kelvin@cloud-consulting.asia>
2020-11-23 12:06:39 +0800

    Update the endpoint URL

--- app/lib/request.js
+++ app/lib/request.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const base = "kinujo-develop.c2sg.asia";
+const base = "kinujo-release.c2sg.asia";
 // const base = "kinujo-demo.c2sg.asia";
 // const base = "http://192.168.0.107:8000";
 let api = "https://" + base + "/api/";
