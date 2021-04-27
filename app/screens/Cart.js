a6cf4c4442457c78b8b1a3c4c6198948e16ec598
koki <koki.shibata@cloud-consulting.asia>
2021-04-27 15:37:21 +0800

    address in cat not showing bug fixed

--- app/screens/Cart.js
+++ app/screens/Cart.js
@@ -133,57 +133,62 @@ export default function Cart(props) {
         </View>
       );
     } else {
-      tmpAddresses.push(
-        <View style={styles.deliveryTabContainer} key={pAddresses.id}>
-          <View
-            style={{
-              position: "absolute",
-              marginLeft: widthPercentageToDP("4%"),
-            }}
-          >
-            <Text style={{ fontSize: RFValue(12) }}>
-              {Translate.t("destination")}
-            </Text>
-            <TouchableWithoutFeedback
-              onPress={() =>
-                props.navigation.navigate("ShippingList", {
-                  type: "cart",
-                })
-              }
-            >
-              <View style={styles.buttonContainer}>
-                <Text style={{ fontSize: RFValue(11), color: "white" }}>
-                  {Translate.t("change")}
+      request
+        .get(pAddresses.prefecture)
+        .then(res => {
+          tmpAddresses.push(
+            <View style={styles.deliveryTabContainer} key={pAddresses.id}>
+              <View
+                style={{
+                  position: "absolute",
+                  marginLeft: widthPercentageToDP("4%"),
+                }}
+              >
+                <Text style={{ fontSize: RFValue(12) }}>
+                  {Translate.t("destination")}
                 </Text>
+                <TouchableWithoutFeedback
+                  onPress={() =>
+                    props.navigation.navigate("ShippingList", {
+                      type: "cart",
+                    })
+                  }
+                >
+                  <View style={styles.buttonContainer}>
+                    <Text style={{ fontSize: RFValue(11), color: "white" }}>
+                      {Translate.t("change")}
+                    </Text>
+                  </View>
+                </TouchableWithoutFeedback>
               </View>
-            </TouchableWithoutFeedback>
-          </View>
-          <View
-            style={{
-              // marginRight: widthPercentageToDP("4%"),
-              position: "absolute",
-              right: 0,
-              justifyContent: "center",
-            }}
-          >
-            <Text
-              style={{
-                fontSize: RFValue(12),
-                // backgroundColor: "orange",
-                width: widthPercentageToDP("45%"),
-              }}
-            >
-              {pAddresses.name}
-            </Text>
-            <Text style={styles.textInTabContainer}>{pAddresses.zip1}</Text>
-            <Text style={styles.textInTabContainer}>
-              {pAddresses.prefecture.name}
-            </Text>
-            <Text style={styles.textInTabContainer}>{pAddresses.address1}</Text>
-            <Text style={styles.textInTabContainer}>{pAddresses.address2}</Text>
-          </View>
-        </View>
-      )
+              <View
+                style={{
+                  // marginRight: widthPercentageToDP("4%"),
+                  position: "absolute",
+                  right: 0,
+                  justifyContent: "center",
+                }}
+              >
+                <Text
+                  style={{
+                    fontSize: RFValue(12),
+                    // backgroundColor: "orange",
+                    width: widthPercentageToDP("45%"),
+                  }}
+                >
+                  {pAddresses.name}
+                </Text>
+                <Text style={styles.textInTabContainer}>{pAddresses.zip1}</Text>
+                <Text style={styles.textInTabContainer}>
+                  {res.data.name}
+                </Text>
+                <Text style={styles.textInTabContainer}>{pAddresses.address1}</Text>
+                <Text style={styles.textInTabContainer}>{pAddresses.address2}</Text>
+              </View>
+            </View>
+          )
+        })
+        
     }
     // return tmpAddresses[0];
     return tmpAddresses;
@@ -791,6 +796,7 @@ export default function Cart(props) {
 
         AsyncStorage.getItem("defaultAddress").then((address) => {
           if (address != null) {
+
             request.get(address).then((response) => {
               // onAddressHtmlChanged(getAddressHtml(response.data, ""));
               // onSelectedChanged(response.data.id);
